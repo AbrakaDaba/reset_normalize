dd7ac4597489a8fb25bb50caff9c04297fea71da
David Damnjanovic <36664588+AbrakaDaba@users.noreply.github.com>
2022-03-28 23:08:05 +0200

    Add files via upload

---
+++ 1-reset-helper.js
@@ -0,0 +1,21 @@
+let choices = document.querySelectorAll('.choice'); 
+
+choices.forEach((el) => {
+    el.addEventListener("click", (elem)=>{
+        removeActive()
+        elem.target.classList.toggle('js-active')
+        let choice = elem.target.innerHTML.toLowerCase() 
+        let linkCssTag = document.querySelector('[rel="stylesheet"]')
+        if(choice !== "user agent stylesheet"){
+            linkCssTag.setAttribute("href",`1-${choice}.css`)
+        }else{
+            linkCssTag.setAttribute("href","")
+        }
+    })
+})
+
+function removeActive(){
+    choices.forEach((el) => {
+        el.classList.remove('js-active')
+    })
+}
